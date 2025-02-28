e4cc5a705883803178b231a7d62548702ea22ddf
Edgar Luque <git@edgl.dev>
2025-02-28 08:35:09 +0100

    better

--- irvm-lower/src/lib.rs
+++ irvm-lower/src/lib.rs
@@ -20,6 +20,8 @@ mod test {
             .add_function("test", &[Parameter::new(Type::Int(32))], Type::Int(32))
             .get_id();
 
+        let test_func_ret_ty = module.get_function(test_func).result_type.clone();
+
         {
             let func = module.get_function_mut(main_func);
             let param = func.param(0)?;
@@ -39,21 +41,23 @@ mod test {
 
             func.blocks[entry_block].instr_cond_jmp(then_block, else_block, &cond, &[], &[]);
 
+            // then block
             {
                 let value = func.blocks[then_block].instr_add(&value, &Operand::const_i32(2))?;
                 func.blocks[then_block].instr_jmp(final_block, &[value]);
             }
 
+            // else block
             {
                 let value = func.blocks[else_block].instr_add(&value, &Operand::const_i32(6))?;
                 func.blocks[else_block].instr_jmp(final_block, &[value]);
             }
 
+            // final block
             {
                 let param = func.blocks[final_block].arg(0)?;
-                let ret_ty = module.get_function(test_func).result_type.clone();
-                let func = module.get_function_mut(main_func);
-                let value = func.blocks[final_block].instr_call(test_func, &[param], &ret_ty)?;
+                let value =
+                    func.blocks[final_block].instr_call(test_func, &[param], &test_func_ret_ty)?;
                 func.blocks[final_block].instr_ret(Some(&value));
             }
         }
